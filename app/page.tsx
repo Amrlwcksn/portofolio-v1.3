09567451bd62556009bab42018fa40f84becdbe5
Amrlwcksn <amrlwcksn@gmail.com>
2025-05-21 10:18:36 +0700

    remove link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import FadeContent from "./components/FadeContent/FadeContent";
 import Clock from "./components/clock";
 import Image from 'next/image';
-import Link from "next/link";
 import { useCallback } from "react";
 
 
